9d815b5e2064132e3fc6e3fc5de642ab400ba477
Pedro Campos <pedrogcamposb@outlook.com>
2021-11-01 20:21:24 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+function checksExistsRepository(request, response, next) {
+  // I got help to solve this particular problem from prenato84's solution at https://github.com/prenato84/ignite-template-corrigindo-o-codigo/blob/main/src/index.js
+
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+
+  return next();
+}
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+}); // DONE
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+}); // DONE
+
+app.put("/repositories/:id", checksExistsRepository, (request, response) => {
+  // I got help to solve this particular problem from prenato84's solution at https://github.com/prenato84/ignite-template-corrigindo-o-codigo/blob/main/src/index.js
+
+  const { title, url, techs } = request.body;
+
+  const { repositoryIndex } = request;
+
+  const repository = { ...repositories[repositoryIndex], title, url, techs };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+}); // DONE
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  ++repositories[repositoryIndex].likes;
+
+  return response.json(repositories[repositoryIndex]);
+}); // DONE
+
+module.exports = app;
